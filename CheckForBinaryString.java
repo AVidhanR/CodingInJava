4cc98f0f35c3a56ff71ff33de9275cffad1f7f94
AVidhanR <itsvidhanreddy@gmail.com>
2025-01-01 20:25:57 +0530

    Check for binary string using java

---
+++ CheckForBinaryString.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class CheckForBinaryString {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the string: ");
+        String s = sc.nextLine();
+        boolean ib = false;
+
+        for (char c : s.toCharArray()) {
+            if (c == '0' || c == '1') {
+                ib = true;
+            } else {
+                ib = false;
+                break;
+            }
+        }
+
+        if (ib == true) {
+            System.out.println(s + " is a binary.");
+        } else {
+            System.out.println(s + " is not a binary.");
+        }
+        sc.close();
+        return;
+    }
+}
