952dd060455fbb84eae9f89d9a83ec097e884351
AVidhanR <itsvidhanreddy@gmail.com>
2025-01-02 21:22:10 +0530

    any case to uppercase convertor

---
+++ GFG_BasicProblems/LowerCaseToUpperCase.java
@@ -0,0 +1,18 @@
+/**
+ * Given a string str containing only lowercase letters,
+ * generate a string with the same letters, but in uppercase
+ */
+import java.util.Scanner;
+
+public class LowerCaseToUpperCase {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the string: ");
+        String str = sc.nextLine();
+
+        System.out.println(str + " => In Upper Case => " + str.toUpperCase());
+        sc.close();
+        return;
+    }
+}
