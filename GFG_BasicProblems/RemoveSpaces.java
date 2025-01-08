2ad97cd703884921699eef292a1fca24d20d911e
AVidhanR <itsvidhanreddy@gmail.com>
2025-01-08 21:39:01 +0530

    remove spaces in a string
    Given a string, remove all spaces from it.

---
+++ GFG_BasicProblems/RemoveSpaces.java
@@ -0,0 +1,18 @@
+/**
+ * Given a string, remove all spaces from it.
+ */
+import java.util.Scanner;
+
+public class RemoveSpaces {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a string: ");
+        String s = sc.nextLine();
+
+        s = s.replace(" ", "");
+
+        System.out.println("After removing spaces: " + s);
+        sc.close();
+    }
+}
