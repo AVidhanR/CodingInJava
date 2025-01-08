30a8f089f40dca2f0cba79d112c7e8ac98dd9840
AVidhanR <itsvidhanreddy@gmail.com>
2025-01-08 21:57:16 +0530

    sum of digits is a palindrome or not
    Given a number n. Return true if the digit sum(or sum of digits) of n is a Palindrome number otherwise false.

---
+++ GFG_BasicProblems/SumOfDigitsIsPalindromeOrNot.java
@@ -0,0 +1,29 @@
+/**
+ * Given a number n. Return true if the digit sum(or sum of digits) of n is a Palindrome number otherwise false.
+ */
+import java.util.Scanner;
+
+public class SumOfDigitsIsPalindromeOrNot {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int n = sc.nextInt(), s = 0, r = 0;
+
+        for (; n != 0; n /= 10) {
+            s += (n % 10);
+        }
+
+        for (n = s; n != 0; n /= 10) {
+            r = (r * 10) + (n % 10);
+        }
+
+        if (r == s) {
+            System.out.println("The Sum Of Digits " + s + " is a palindrome");
+        } else {
+            System.out.println("The Sum Of Digits " + s + " is not a palindrome");
+        }
+
+        sc.close();
+    }
+}
