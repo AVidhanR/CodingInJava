ad02a7c44688a075b21b94416e5936d72a4259f0
AVidhanR <itsvidhanreddy@gmail.com>
2025-01-17 12:33:17 +0530

    find factorial...

---
+++ Arithmetics/FindFactorial.java
@@ -0,0 +1,15 @@
+/**
+ * Calculate factorial of a given number N.
+ */
+import java.util.Scanner;
+
+public class FindFactorial {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    System.out.print("Enter a number: ");
+    int n = sc.nextInt();
+
+
+  }
+}
