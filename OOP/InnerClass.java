637d2f0dcaf4adb24e6eb543dd60cfbc0d8777ac
AVidhanR <itsvidhanreddy@gmail.com>
2025-01-19 21:23:49 +0530

    inner class and it's variants

---
+++ OOP/InnerClass.java
@@ -0,0 +1,39 @@
+/**
+ * OOP Concept: Inner Classes [RECAP]
+ */
+
+// can't make the outer class static
+class Database {
+  String connectionString;
+
+  public void show() {
+    System.out.println(connectionString + " is the db");
+  }
+
+  class DBConfig {
+    public void config() {
+      System.out.println("DB Configuration");
+    }
+  }
+
+  static class Platform {
+    public void showPlatform() {
+      System.out.println("Platform is Azure");
+    }
+  }
+}
+
+public class InnerClass {
+  public static void main(String[] args) {
+    Database db = new Database();
+    db.connectionString = "MongoDB";
+    db.show();
+
+    Database.DBConfig configs = db.new DBConfig();
+    configs.config();
+
+    // is possible if the inner class is static
+    Database.Platform platform = new Database.Platform();
+    platform.showPlatform();
+  }
+}
