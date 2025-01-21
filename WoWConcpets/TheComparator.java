822a2be6725fa40c1565c104e038ab513549ae4a
AVidhanR <itsvidhanreddy@gmail.com>
2025-01-21 17:50:52 +0530

    this is really helpful in many ways - The Comparable

---
+++ WoWConcpets/TheComparator.java
@@ -0,0 +1,38 @@
+import java.util.*;
+
+class Students implements Comparable<Students> {
+  int age;
+  String name;
+
+  public Students(int age, String name) {
+    this.age = age;
+    this.name = name;
+  }
+
+  @Override
+  public int compareTo(Students o1) {
+    if (this.age > o1.age)
+      return 1;
+    else
+      return 0;
+  }
+
+  @Override
+  public String toString() {
+    return "Students{" +
+        "age=" + age +
+        ", name='" + name + '\'' +
+        '}';
+  }
+}
+
+public class TheComparator {
+  public static void main(String[] args) {
+    List<Students> studs = new ArrayList<>();
+    studs.add(new Students(21, "AVidhanR"));
+    studs.add(new Students(22, "AVin"));
+    studs.add(new Students(19, "AVReddy"));
+    Collections.sort(studs);
+    System.out.println(studs);
+  }
+}
