421a298301d97003a52a976115afb05a9e94589f
AVidhanR <itsvidhanreddy@gmail.com>
2025-01-21 15:20:20 +0530

    as always Scanner is the best

--- WoWConcpets/TypesOfInputTaking.java
+++ WoWConcpets/TypesOfInputTaking.java
@@ -5,6 +5,7 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Scanner;
 
 public class TypesOfInputTaking {
   public static void main(String[] args) throws IOException {
@@ -13,9 +14,14 @@ public class TypesOfInputTaking {
     InputStreamReader isr = new InputStreamReader(System.in);
     BufferedReader br = new BufferedReader(isr);
     int num = Integer.parseInt(br.readLine());
-
     System.out.println(num);
 
+    System.out.println("Enter another number: ");
+    Scanner sc = new Scanner(System.in);
+    int num2 = sc.nextInt();
+    System.out.println(num2);
+
+    sc.close();
     isr.close();
     br.close();
   }
