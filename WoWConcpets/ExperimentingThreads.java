f4b621740a1abe5649b1e1832f3a8fa57710c619
AVidhanR <itsvidhanreddy@gmail.com>
2025-01-21 16:05:11 +0530

    experimenting on threads
    using lambda expressions is the best in threads

--- WoWConcpets/ExperimentingThreads.java
+++ WoWConcpets/ExperimentingThreads.java
@@ -2,36 +2,26 @@
  * Threads Concept: The OP Thing in java
  */
 
-class ThreadOne implements Runnable {
-  @Override
-  public void run() {
-    for (int i = 1; i <= 100; i++) {
-      System.out.println(i + "th Hi");
-      try {
-        Thread.sleep(10);
-      } catch (InterruptedException e) { e.printStackTrace(); }
-    }
-  }
-}
-
-class ThreadTwo implements Runnable {
-  @Override
-  public void run() {
-    for (int i = 1; i <= 100; i++) {
-      System.out.println(i + "th Hello");
-      try {
-        Thread.sleep(10);
-      } catch (InterruptedException e) { e.printStackTrace(); }
-    }
-  }
-}
-
 public class ExperimentingThreads {
   public static void main(String[] args) {
-    Runnable r1 = new ThreadOne();
-    Runnable r2 = new ThreadTwo();
 
-    new Thread(r1).start();
-    new Thread(r2).start();
+    /** using lambda expressions; as Runnable interface is a {@FunctionalInterface} */
+    new Thread(() -> {
+    for (int i = 1; i <= 100; i++) {
+        System.out.println(i + "th Hello");
+        try {
+          Thread.sleep(10);
+        } catch (InterruptedException e) { e.printStackTrace(); }
+      }
+    }).start();
+
+    new Thread(() -> {
+    for (int i = 1; i <= 100; i++) {
+        System.out.println(i + "th Hello");
+        try {
+          Thread.sleep(10);
+        } catch (InterruptedException e) { e.printStackTrace(); }
+      }
+    }).start();
   }
 }
