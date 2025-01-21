f296215833b960096919c7b160440785da317506
AVidhanR <itsvidhanreddy@gmail.com>
2025-01-21 18:00:11 +0530

    just use lambda expr guys!
    just go look the code, right after the main method

--- WoWConcpets/TheComparator.java
+++ WoWConcpets/TheComparator.java
@@ -28,11 +28,16 @@ class Students implements Comparable<Students> {
 
 public class TheComparator {
   public static void main(String[] args) {
+    Comparator<Students> comp = (o1, o2) -> (o1.age > o2.age) ? 1 : 0;
+
     List<Students> studs = new ArrayList<>();
     studs.add(new Students(21, "AVidhanR"));
     studs.add(new Students(22, "AVin"));
     studs.add(new Students(19, "AVReddy"));
     Collections.sort(studs);
+    System.out.println(studs);
+    // or
+    Collections.sort(studs, comp);
     System.out.println(studs);
   }
 }
