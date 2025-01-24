3971d3f216bafc6b591723ad44dfa1389a7461a1
AVidhanR <itsvidhanreddy@gmail.com>
2025-01-24 22:10:38 +0530

    trying out the new features in java

---
+++ WoWConcpets/NewFeaturesInJava.java
@@ -0,0 +1,16 @@
+/**
+ * Below are the new @features of java,
+ *  - [var] type inference (New Keyword)
+ *  - [sealed] classes (New Concept in OOP)
+ *  -
+ */
+
+public class NewFeaturesInJava {
+  public static void main(String[] args) {
+    // can't use var as a className here
+    var a = 10;
+    System.out.println("Printing the var declared value: " + a);
+
+
+  }
+}
