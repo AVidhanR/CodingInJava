bf50c5c144facfff3da69321b30e10a87fcac715
AVidhanR <itsvidhanreddy@gmail.com>
2025-01-25 09:59:40 +0530

    sealed class concept just what I learned from Kotlin

--- WoWConcpets/NewFeaturesInJava.java
+++ WoWConcpets/NewFeaturesInJava.java
@@ -2,8 +2,19 @@
  * Below are the new @features of java,
  *  - [var] type inference (New Keyword)
  *  - [sealed] classes (New Concept in OOP)
+ *    - It's used to restrict the class that stands between [abstract] class and [final] class.
  *  -
  */
+
+sealed class ClassOne permits ClassTwo, ClassThree {
+  void show() {
+    System.out.println("In Sealed ClassOne");
+  }
+}
+
+// sealed or non-sealed or final needed to added for permitting classes
+non-sealed class ClassTwo extends ClassOne {}
+non-sealed class ClassThree extends ClassOne {}
 
 public class NewFeaturesInJava {
   public static void main(String[] args) {
