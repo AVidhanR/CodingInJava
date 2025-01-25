a44e28e2fa04b9c12a0ee80b8e270f54547a2cbe
AVidhanR <itsvidhanreddy@gmail.com>
2025-01-25 16:35:56 +0530

    linked list implemetation initiated

---
+++ src/main/java/dev/itsvidhanreddy/DSA/LinkedListImplementation.java
@@ -0,0 +1,13 @@
+/**
+ * Data Structures and Algorithms: Linked List Data Structure implementation
+ */
+
+package dev.itsvidhanreddy.DSA;
+
+import java.util.Scanner;
+
+public class LinkedListImplementation {
+  public static void main(String[] args) {
+
+  }
+}
