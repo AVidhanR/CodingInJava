de927eaccdd55b76417c1514a7f0b10c17cce51e
AVidhanR <itsvidhanreddy@gmail.com>
2025-01-26 12:11:58 +0530

    create tree data structure imple' file

---
+++ src/main/java/dev/itsvidhanreddy/DSA/Tree.java
@@ -0,0 +1,11 @@
+/**
+ * Data Structures and Algorithms: Tree Data Structure implementation [NEW]
+ */
+
+package dev.itsvidhanreddy.DSA;
+
+public class Tree {
+  public static void main(String[] args) {
+
+  }
+}
